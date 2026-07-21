3be3fd0823c6c9803e59471ef2a773ec7fbb07c0
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-07-22 00:03:17 +0300

    feat: configure JWT authentication Foundation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => ({
+      secret: config.getOrThrow<string>('JWT_SECRET'),
+      signOptions: {
+      expiresIn: config.getOrThrow<string>('JWT_EXPIRES_IN'),
+    },
+  }),
+})
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class AuthModule {}
