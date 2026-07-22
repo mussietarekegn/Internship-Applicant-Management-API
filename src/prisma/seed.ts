2fce70cf0743f6780546a30ddca2bac179719b1f
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-07-22 11:25:53 +0300

    feat: add admin seed.

---
+++ src/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await bcrypt.hash('admin123', 10);
+
+  await prisma.admin.upsert({
+    where: {
+      email: 'admin@infnova.com',
+    },
+    update: {},
+    create: {
+      email: 'admin@infnova.com',
+      password,
+    },
+  });
+
+  console.log(' Admin seeded');
+}
+
+main()
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
