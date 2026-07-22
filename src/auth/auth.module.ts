01cdef250c2a751a6756d7be907ba446c7652894
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-07-22 11:36:08 +0300

    feat: done the auth module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,45 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+import { PrismaModule } from '../prisma/prisma.module';
+
 @Module({
   imports: [
-    ConfigModule,
+    PrismaModule,
+
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+
     JwtModule.registerAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: async (config: ConfigService) => ({
-    secret: config.getOrThrow<string>('JWT_SECRET'),
-    signOptions: {
-    expiresIn: config.getOrThrow<string>('JWT_EXPIRES_IN') as any,
-  },
-}),
-})
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow<string>('JWT_SECRET'),
+
+        signOptions: {
+          expiresIn: config.getOrThrow('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
   ],
+
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+
+  providers: [
+    AuthService,
+  ],
+
+  exports: [
+    AuthService,
+    PassportModule,
+    JwtModule,
+  ],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +14,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Internship Applicant Management API')
+    .setDescription('Backend Internship Challenge - INFNOVA Technologies')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
